3959c85b00b8ab77c7155144ff6099b389cfe2c0
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-25 11:08:04 -0300

    :sparkles: feat: Criadas classes de fluxo de produtos no carrinho

---
+++ src/main/java/br/dev/paulowolfgang/exemplos/loja_virtual/CarrinhoDeCompras.java
@@ -0,0 +1,27 @@
+package br.dev.paulowolfgang.exemplos.loja_virtual;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class CarrinhoDeCompras {
+
+    private final List<Produto> produtos;
+
+    public CarrinhoDeCompras() {
+        this.produtos = new ArrayList<>();
+    }
+
+    public void adiciona(Produto produto) {
+        if (produto != null) {
+            this.produtos.add(produto);
+        }
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+}
---
+++ src/main/java/br/dev/paulowolfgang/exemplos/loja_virtual/Principal.java
@@ -0,0 +1,20 @@
+package br.dev.paulowolfgang.exemplos.loja_virtual;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class Principal {
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.adiciona(new Produto("Liquidificador", 250.0));
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+        carrinho.adiciona(new Produto("Jogo de pratos", 70.0));
+        
+        MaiorEMenor algoritmo = new MaiorEMenor();
+        algoritmo.encontra(carrinho);
+        
+        System.out.println("O maior produto: " + algoritmo.getMaior().getNome());
+        System.out.println("O menor produto: " + algoritmo.getMenor().getNome());
+    }
+}
