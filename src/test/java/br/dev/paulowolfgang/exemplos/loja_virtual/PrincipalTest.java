daa1dd3752dbfbc19523bc21e87b77e0dc287643
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-25 11:09:29 -0300

    :test_tube: test: Criada classe de teste de produtos com maior e menor valor

---
+++ src/test/java/br/dev/paulowolfgang/exemplos/loja_virtual/PrincipalTest.java
@@ -0,0 +1,69 @@
+package br.dev.paulowolfgang.exemplos.loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class PrincipalTest {
+    
+    @Test
+    public void apenasUmProduto()
+    {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+        
+        MaiorEMenor maior_e_menor = new MaiorEMenor();
+        maior_e_menor.encontra(carrinho);
+        
+        Assertions.assertEquals("Geladeira", maior_e_menor.getMaior().getNome());
+        Assertions.assertEquals("Geladeira", maior_e_menor.getMenor().getNome());
+    }
+    
+    @Test
+    public void ordemCrescente()
+    {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.adiciona(new Produto("Jogo de pratos", 70.0));
+        carrinho.adiciona(new Produto("Liquidificador", 250.0));
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+        
+        MaiorEMenor maior_e_menor = new MaiorEMenor();
+        maior_e_menor.encontra(carrinho);
+        
+        Assertions.assertEquals("Geladeira", maior_e_menor.getMaior().getNome());
+        Assertions.assertEquals("Jogo de pratos", maior_e_menor.getMenor().getNome());
+    }
+    
+    @Test
+    public void ordemDecrescente()
+    {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+        carrinho.adiciona(new Produto("Liquidificador", 250.0));
+        carrinho.adiciona(new Produto("Jogo de pratos", 70.0));
+        
+        MaiorEMenor maior_e_menor = new MaiorEMenor();
+        maior_e_menor.encontra(carrinho);
+        
+        Assertions.assertEquals("Geladeira", maior_e_menor.getMaior().getNome());
+        Assertions.assertEquals("Jogo de pratos", maior_e_menor.getMenor().getNome());
+    }
+    
+    @Test
+    public void produtoNaoOrdenado()
+    {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.adiciona(new Produto("Liquidificador", 250.0));
+        carrinho.adiciona(new Produto("Geladeira", 450.0));
+        carrinho.adiciona(new Produto("Jogo de pratos", 70.0));
+        
+        MaiorEMenor maior_e_menor = new MaiorEMenor();
+        maior_e_menor.encontra(carrinho);
+        
+        Assertions.assertEquals("Geladeira", maior_e_menor.getMaior().getNome());
+        Assertions.assertEquals("Jogo de pratos", maior_e_menor.getMenor().getNome());
+    }
+}
