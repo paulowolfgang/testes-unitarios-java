8e288cc394f1b851a9d7e821835372a6ac5a6f1d
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-25 16:30:45 -0300

    :sparkles: feat: Ajustada classe de conversão de número para tratar o simbolo X e outros valores isolados

--- src/main/java/br/dev/paulowolfgang/exemplos/conversor/ConversorDeNumeroRomano.java
+++ src/main/java/br/dev/paulowolfgang/exemplos/conversor/ConversorDeNumeroRomano.java
@@ -1,19 +1,26 @@
 package br.dev.paulowolfgang.exemplos.conversor;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Paulo Silva
  */
 public class ConversorDeNumeroRomano {
     
+    private static HashMap<String, Integer> tabelaSimbolos = new HashMap<>()
+    {{
+        put("I", 1);
+        put("V", 5);
+        put("X", 10);
+        put("L", 50);
+        put("C", 100);
+        put("D", 500);
+        put("M", 1000);
+    }};
+            
     public int converte(String numeroEmRomano)
     {
-        if(numeroEmRomano.equals("I"))
-        {
-            return 1;
-        }else if(numeroEmRomano.equals("V")){
-            return 5;
-        }
-        return 0;
+        return tabelaSimbolos.get(numeroEmRomano);
     }
 }
