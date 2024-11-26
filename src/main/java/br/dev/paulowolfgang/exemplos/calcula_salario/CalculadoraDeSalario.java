7f9819a3ed1455141340d963e5ab73bba0199b10
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-26 16:21:14 -0300

    :sparkles: feat: Criada classe de cálculo de salário

---
+++ src/main/java/br/dev/paulowolfgang/exemplos/calcula_salario/CalculadoraDeSalario.java
@@ -0,0 +1,13 @@
+package br.dev.paulowolfgang.exemplos.calcula_salario;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class CalculadoraDeSalario
+{
+    public double calculaSalario(Funcionario funcionario)
+    {
+        return 1350.0;
+    }
+}
