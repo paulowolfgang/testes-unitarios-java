424dd64f131c48936246fe2682c90c9e2978e336
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-26 10:45:29 -0300

    :sparkles: feat: Ajustada classe de conversão de número para tratar simbolos em sequência

--- src/main/java/br/dev/paulowolfgang/exemplos/conversor/ConversorDeNumeroRomano.java
+++ src/main/java/br/dev/paulowolfgang/exemplos/conversor/ConversorDeNumeroRomano.java
@@ -8,19 +8,24 @@ import java.util.HashMap;
  */
 public class ConversorDeNumeroRomano {
     
-    private static HashMap<String, Integer> tabelaSimbolos = new HashMap<>()
+    private static final HashMap<Character, Integer> tabelaSimbolos = new HashMap<>()
     {{
-        put("I", 1);
-        put("V", 5);
-        put("X", 10);
-        put("L", 50);
-        put("C", 100);
-        put("D", 500);
-        put("M", 1000);
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
     }};
             
     public int converte(String numeroEmRomano)
     {
-        return tabelaSimbolos.get(numeroEmRomano);
+        int acumulador = 0;
+        for(int i = 0; i < numeroEmRomano.length(); i++)
+        {
+            acumulador += tabelaSimbolos.get(numeroEmRomano.charAt(i));
+        }
+        return acumulador;
     }
 }
