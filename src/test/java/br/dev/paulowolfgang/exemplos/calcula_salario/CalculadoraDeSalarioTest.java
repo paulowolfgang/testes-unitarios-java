6d51f9dbcdb2aaacccebfe6bb2612fc4759ee8c9
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-26 16:43:48 -0300

    :test_tube: test: Criado método de teste para o caso do dba com salário abaixo do limite

--- src/test/java/br/dev/paulowolfgang/exemplos/calcula_salario/CalculadoraDeSalarioTest.java
+++ src/test/java/br/dev/paulowolfgang/exemplos/calcula_salario/CalculadoraDeSalarioTest.java
@@ -28,4 +28,14 @@ public class CalculadoraDeSalarioTest {
         double salario = calculadora.calculaSalario(desenvolvedor);
         Assertions.assertEquals(4000.0 * 0.8, salario);
     }
+    
+    @Test
+    public void deveCalcularSalarioParaDbasComSalarioAbaixoDoLimite()
+    {
+        CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
+        Funcionario dba = new Funcionario("José", 1000.0, Cargo.DBA);
+        
+        double salario = calculadora.calculaSalario(dba);
+        Assertions.assertEquals(1000.0 * 0.85, salario);
+    }
 }
