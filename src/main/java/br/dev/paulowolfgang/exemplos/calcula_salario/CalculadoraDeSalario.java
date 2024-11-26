2b720c2337e88e032ed7acb1bbc15b7abecd7aa8
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-26 16:36:17 -0300

    :sparkles: feat: Ajustada classe de cálculo de salário para suportar desenvolvedores que ganham acima do limite

--- src/main/java/br/dev/paulowolfgang/exemplos/calcula_salario/CalculadoraDeSalario.java
+++ src/main/java/br/dev/paulowolfgang/exemplos/calcula_salario/CalculadoraDeSalario.java
@@ -8,6 +8,10 @@ public class CalculadoraDeSalario
 {
     public double calculaSalario(Funcionario funcionario)
     {
+        if(funcionario.getSalario() > 3000)
+        {
+            return 3200.0;
+        }
         return 1350.0;
     }
 }
