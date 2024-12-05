f27afd4d81bd2c3d082588206bc8b8659f9d6c63
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-05 12:53:27 -0300

    :sparkles: feat: Criada classe de maior preço

---
+++ src/main/java/br/dev/paulowolfgang/exemplos/loja_virtual/exemplo02/MaiorPreco.java
@@ -0,0 +1,13 @@
+package br.dev.paulowolfgang.exemplos.loja_virtual.exemplo02;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class MaiorPreco {
+    
+    public double encontra(CarrinhoDeCompras carrinho)
+    {
+        return  0;
+    }
+}
