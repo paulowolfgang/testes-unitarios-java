824c559380c15f3608582e3fc897769be96364af
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-07 20:08:41 -0300

    :recycle: refactor: Refatoradas classe calculadora de salário e enum de cargo

--- src/main/java/br/dev/paulowolfgang/exemplos/calcula_salario/CalculadoraDeSalario.java
+++ src/main/java/br/dev/paulowolfgang/exemplos/calcula_salario/CalculadoraDeSalario.java
@@ -6,42 +6,8 @@ package br.dev.paulowolfgang.exemplos.calcula_salario;
  */
 public class CalculadoraDeSalario
 {
-    private static final double DESENVOLVEDOR_LIMITE_SALARIO = 3000;
-    private static final double DESENVOLVEDOR_VALOR_DESCONTO_ACIMA_LIMITE = 0.8;
-    private static final double DESENVOLVEDOR_VALOR_DESCONTO_ABAIXO_LIMITE = 0.9;
-    
-    private static final double DBA_LIMITE_SALARIO = 2500;
-    private static final double DBA_VALOR_DESCONTO_ACIMA_LIMITE = 0.75;
-    private static final double DBA_VALOR_DESCONTO_ABAIXO_LIMITE = 0.85;
-    
-    private static final double TESTADOR_LIMITE_SALARIO = 2000;
-    private static final double TESTADOR_VALOR_DESCONTO_ACIMA_LIMITE = 0.1;
-    private static final double TESTADOR_VALOR_DESCONTO_ABAIXO_LIMITE = 0.05;
-    
     public double calculaSalario(Funcionario funcionario)
     {
-        switch (funcionario.getCargo()) {
-            case DESENVOLVEDOR -> {
-                if(funcionario.getSalario() > DESENVOLVEDOR_LIMITE_SALARIO)
-                {
-                    return funcionario.getSalario() * DESENVOLVEDOR_VALOR_DESCONTO_ACIMA_LIMITE;
-                }
-                return funcionario.getSalario() * DESENVOLVEDOR_VALOR_DESCONTO_ABAIXO_LIMITE;
-            }
-            case DBA -> {
-                if(funcionario.getSalario() > DBA_LIMITE_SALARIO)
-                {
-                    return funcionario.getSalario() * DBA_VALOR_DESCONTO_ACIMA_LIMITE;
-                }
-                return funcionario.getSalario() * DBA_VALOR_DESCONTO_ABAIXO_LIMITE;
-            }
-            default -> {
-                if(funcionario.getSalario() > TESTADOR_LIMITE_SALARIO)
-                {
-                    return funcionario.getSalario() * TESTADOR_VALOR_DESCONTO_ACIMA_LIMITE;
-                }
-                return funcionario.getSalario() * TESTADOR_VALOR_DESCONTO_ABAIXO_LIMITE;
-            }
-        }
+        return funcionario.getCargo().getRegra().calcula(funcionario);
     }
 }
--- src/main/java/br/dev/paulowolfgang/exemplos/calcula_salario/Cargo.java
+++ src/main/java/br/dev/paulowolfgang/exemplos/calcula_salario/Cargo.java
@@ -5,7 +5,20 @@ package br.dev.paulowolfgang.exemplos.calcula_salario;
  * @author Paulo Silva
  */
 public enum Cargo {
-    DESENVOLVEDOR,
-    DBA,
-    TESTADOR
+    
+    DESENVOLVEDOR(new DezOuVintePorCento()),
+    DBA(new QuinzeOuVinteCincoPorCentro()),
+    TESTADOR(new CincoOuDezPorCento());
+    
+    private final RegraDeCalculo regra;
+    
+    Cargo(RegraDeCalculo regra)
+    {
+        this.regra = regra;
+    }
+    
+    public RegraDeCalculo getRegra()
+    {
+        return regra;
+    }
 }
