480a5c0709417463220d43f6f09ba28105503b9a
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-08 20:14:47 -0300

    :sparkles: feat: Criadas classes auxiliares

---
+++ src/main/java/br/dev/paulowolfgang/exemplos/notal_fiscal/NfDao.java
@@ -0,0 +1,14 @@
+package br.dev.paulowolfgang.exemplos.notal_fiscal;
+
+/**
+ *
+ * @author Paulo SIlva
+ */
+public class NfDao {
+    
+    public void persiste(NotaFiscal notaFiscal)
+    {
+        // Simulação do fluxo de persistência de NF na base de dados
+        System.out.println("Persistindo NF no banco de dados..");
+    }
+}
---
+++ src/main/java/br/dev/paulowolfgang/exemplos/notal_fiscal/Sap.java
@@ -0,0 +1,14 @@
+package br.dev.paulowolfgang.exemplos.notal_fiscal;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class Sap {
+    
+    public void envia(NotaFiscal notaFiscal)
+    {
+        // Simulação do fluxo de envio de NF para o SAP
+        System.out.println("Enviando NF para o SAP..");
+    }
+}
