1e944ff9cb2e3e7929e34ddf059fe940aca1b42b
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-08 20:09:54 -0300

    :sparkles: feat: Criada classe de nota fiscal

---
+++ src/main/java/br/dev/paulowolfgang/exemplos/notal_fiscal/NotaFiscal.java
@@ -0,0 +1,36 @@
+package br.dev.paulowolfgang.exemplos.notal_fiscal;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class NotaFiscal {
+    
+    private String cliente;
+    private double valor;
+    private Calendar data;
+    
+    public NotaFiscal(String cliente, double valor, Calendar data)
+    {
+        this.cliente = cliente;
+        this.valor = valor;
+        this.data = data;
+    }
+    
+    public String getCliente()
+    {
+        return cliente;
+    }
+    
+    public double getValor()
+    {
+        return valor;
+    }
+    
+    public Calendar getData()
+    {
+        return data;
+    }
+}
