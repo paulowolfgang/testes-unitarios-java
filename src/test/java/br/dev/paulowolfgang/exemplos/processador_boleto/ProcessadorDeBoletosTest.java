dc68000c2e4b60e034ebd7da73dd847357eb4fb6
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-11 10:45:20 -0300

    :test_tube: test: Criado método de teste para o caso de pagamento com múltiplos boletos

--- src/test/java/br/dev/paulowolfgang/exemplos/processador_boleto/ProcessadorDeBoletosTest.java
+++ src/test/java/br/dev/paulowolfgang/exemplos/processador_boleto/ProcessadorDeBoletosTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Assertions;
 public class ProcessadorDeBoletosTest {
     
     @Test
-    public void deveProcessarPagamentoViaBOletoUnico()
+    public void deveProcessarPagamentoViaBoletoUnico()
     {
         ProcessadorDeBoletos processador = new ProcessadorDeBoletos();
         Fatura fatura = new Fatura("Ana", 150.0);
@@ -24,5 +24,22 @@ public class ProcessadorDeBoletosTest {
         
         Assertions.assertEquals(1, fatura.getPagamentos().size());
         Assertions.assertEquals(150.0, fatura.getPagamentos().get(0).getValor());
+    }
+    
+    @Test
+    public void deveProcessarPagamentoViaMuitosBoletos()
+    {
+        ProcessadorDeBoletos processador = new ProcessadorDeBoletos();
+        
+        Fatura fatura = new Fatura("Gisele", 300.0);
+        Boleto boleto01 = new Boleto(100.0);
+        Boleto boleto02 = new Boleto(200.0);
+        List<Boleto> boletos = Arrays.asList(boleto01, boleto02);
+        
+        processador.processa(boletos, fatura);
+        
+        Assertions.assertEquals(2, fatura.getPagamentos().size());
+        Assertions.assertEquals(100.0, fatura.getPagamentos().get(0).getValor());
+        Assertions.assertEquals(200.0, fatura.getPagamentos().get(1).getValor());
     }
 }
