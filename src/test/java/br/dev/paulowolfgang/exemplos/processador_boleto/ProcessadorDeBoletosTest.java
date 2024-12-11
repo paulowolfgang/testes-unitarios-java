4448a413a5988c0e53c939fa643904af47357dfc
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-11 08:38:14 -0300

    :test_tube: test: Criado método de teste para o caso de pagamento com boleto único

---
+++ src/test/java/br/dev/paulowolfgang/exemplos/processador_boleto/ProcessadorDeBoletosTest.java
@@ -0,0 +1,27 @@
+package br.dev.paulowolfgang.exemplos.processador_boleto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ *
+ * @author Paulo Silva
+ */
+public class ProcessadorDeBoletosTest {
+    
+    @Test
+    public void deveProcessarPagamentoViaBOletoUnico()
+    {
+        ProcessadorDeBoletos processador = new ProcessadorDeBoletos();
+        
+        Fatura fatura = new Fatura("Cliente", 150.0);
+        Boleto boleto01 = new Boleto(150.0);
+        
+        List<Boleto> boletos = Arrays.asList(boleto01);
+        
+        processador.processa(boletos, fatura);
+        
+        Assertions.assertEquals(1, fatura.getPagamentos().size());
+        Assertions.assertEquals(150.0, fatura.getPagamentos().get(0).getValor());
+    }
+}
